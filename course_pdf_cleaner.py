1d06cd5444bff5e490c5bd4d3019a91cb9d1f98b
Ix76y <60822770+Ix76y@users.noreply.github.com>
2022-01-09 16:43:56 +0000

    Added simple remove_watermark function

--- course_pdf_cleaner.py
+++ course_pdf_cleaner.py
@@ -8,3 +8,26 @@ def decrypt(inFile, outFile, password):
   pdf = pikepdf.open(inFile, password=password)
   pdf.save(outFile)
   
+def remove_watermark(in_file, out_file, watermark=''):
+  source = PdfFileReader(open(in_file, 'rb'))
+  output = PdfFileWriter()
+  
+  for page in range(source.getNumPages()):
+    page = source.getPage(page)
+    content_object = page["\Contents"].getObject()
+    content = ContentStream(content_object, source)
+    
+    for operands, operator in content.operations:
+      if operator == b_("Tj") or operator == b_("TJ"):
+        # TODO: add logic here if you only want to replace specific strings
+        # like comparing with the passed watermark and only removing then
+        operands[0] = TextStringObject(' ')
+        
+    page.__setitem__(NameObject('\Contents'), content)
+    output.addPage(page)
+  
+  output_stream = open(out_file, 'wb')
+  output.write(output_stream)
+  output_stream.close()
+  
+                   
